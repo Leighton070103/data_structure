24e1f2507cc24c6c4aaa11d1d4d75008d5dc044d
Leighton070103 <leighton070103@gmail.com>
2019-04-25 11:11:26 +0800

    Add comments to merge sort and insertion sort

--- data_structure_and_algorithms/src/sort/InsertionSort.java
+++ data_structure_and_algorithms/src/sort/InsertionSort.java
@@ -1,16 +1,27 @@
 package sort;
 
-
+/**
+ * 插入排序：将数组分为两部分，一部分为已排好序，另一部分为未被排好序的部分。每次将排好序的区域拓展一个index，然后将未排好序区域的第一个数插入排好序的
+ * 区域。
+ */
 public class InsertionSort extends Sort{
     public InsertionSort(int[] arr){
         super(arr);
     }
+
+    /**
+     * 设置index为有序区域的拓展位置；
+     * 设置value记录当前被进行insertion sort的数的值；
+     * 设置mark为该数调整后应被插入的位置，默认值为当前位置；
+     * 在mark位置的前一个数比当前数大的情况下，不断前移mark，直到前一个数比value小，此时mark位置为value应被插入的位置。
+     * 同时，在比较过程中，mark-1位的数如果比mark位大，说明当将value插入之后，mark-1位的数应在mark位上，要向后移一位。
+     */
     @Override
     public void sort() {
         if (arr == null || arr.length < 2) return;
         for (int index = 1; index < arr.length; index ++){
             int mark = index;
-            int value = arr[index];
+            int value = arr[index];//当前数的值
             while (mark > 0 && arr[mark-1] > value) arr[mark] = arr[--mark]; //找到第一个比value大的数的位置。
             arr[mark] = value;
         }
--- data_structure_and_algorithms/src/sort/MergeSort.java
+++ data_structure_and_algorithms/src/sort/MergeSort.java
@@ -1,7 +1,7 @@
 package sort;
 
 /**
- * TODO：内部缓存法
+ * 归并排序：将数组分为2个一组，相邻数组互相比较之后再合并。
  */
 public class MergeSort extends Sort {
 
@@ -14,6 +14,11 @@ public class MergeSort extends Sort {
         sort(0, arr.length-1);
     }
 
+    /**
+     * 将数组一分为二，对左右分别进行再次分割，然后将分割好的数组进行归并。
+     * @param left
+     * @param right
+     */
     private void sort(int left, int right){
         if (left == right) return;
         int mid = (left + right ) / 2;
@@ -22,6 +27,18 @@ public class MergeSort extends Sort {
         merge(left, mid, mid +1, right);
     }
 
+    /**
+     * 创建一个临时数组，用于存放merge好的数。
+     * 对左右数组分别创建指针，对临时数组创建指针；
+     * 比较左右指针所指向的数，将较小的一个数放入临时数组中，并把相应指针后移一位；
+     * 当左右指针任何一个到达终点时，跳出循环；
+     * 将未遍历完的数组直接塞入剩下的临时数组中。
+     * 将临时数组的数重新导入原数组。
+     * @param lStart 左边数组的开始
+     * @param lEnd 左边数组的最后一个数
+     * @param rStart 右边数组的第一个数
+     * @param rEnd 右边数组的最后一个数
+     */
     private void merge(int lStart, int lEnd, int rStart, int rEnd){
         int[] tmp = new int[rEnd - lStart+1];
         int lPtr = lStart;
